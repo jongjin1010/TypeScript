25ca91573ab4ec0f3477bdea64c6fa00fc314cd2
jongjin1010 <jongjin024@gmail.com>
2022-08-06 16:27:25 +0900

    기본 타입

--- src/practice.ts
+++ src/practice.ts
@@ -1,2 +1,16 @@
+let count: number = 0;
+count += 1;
+
 const message: string = 'hello world';
-console.log(message);
+const done: boolean = false;
+
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ['hello', 'world'];
+
+messages.push('hi'); 
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+let color: 'red' | 'green' = 'red';
+color = 'green';

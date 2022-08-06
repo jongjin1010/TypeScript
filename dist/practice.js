9401b4b25766403834c0095f35b596d066f66970
jongjin1010 <jongjin024@gmail.com>
2022-08-06 18:02:49 +0900

    function

--- dist/practice.js
+++ dist/practice.js
@@ -1,14 +1,12 @@
 "use strict";
 let count = 0;
 count += 1;
-count = '문자열'; //error
 const message = 'hello world';
 const done = false;
 const numbers = [1, 2, 3];
 const messages = ['hello', 'world'];
-messages.push(1); //error
+messages.push('hi');
 let mightBeUndefined = undefined;
 let nullableNumber = null;
 let color = 'red';
 color = 'green';
-color = 'yello'; //error
--- src/practice.ts
+++ src/practice.ts
@@ -1,16 +1,5 @@
-let count: number = 0;
-count += 1;
+function sum(x: number, y: number): number {
+  return x + y;
+}
 
-const message: string = 'hello world';
-const done: boolean = false;
-
-const numbers: number[] = [1, 2, 3];
-const messages: string[] = ['hello', 'world'];
-
-messages.push('hi'); 
-
-let mightBeUndefined: string | undefined = undefined;
-let nullableNumber: number | null = null;
-
-let color: 'red' | 'green' = 'red';
-color = 'green';
+sum(1, 2);
